aeff7281433d9f645739dbc5f9543cc8bfa1d350
Yanira Sanchez <yani@Yaniras-MacBook-Pro.local>
2022-10-01 21:25:04 -0500

    added some styling to details page

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -4,10 +4,13 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
+
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
         // 👆 pulls the id parameter used to route to the detail page
+
+import './MovieDetails.css'
 
 
 export default function MovieDetails(){
@@ -30,26 +33,27 @@ export default function MovieDetails(){
         }
     },[movieId])
     // if 👆 changes, then the return function will run which clears the reducer so there is no previous data when the page is loaded for another id. 
-    console.log(movieDetails);
+    // console.log(movieDetails);
     return(
-        <Card sx={{ display: 'flex' }}>
-            <img src={movieDetails.poster}/>
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <CardContent sx={{ flex: '1 0 auto' }}>
-                <Typography component="div" variant="h4">
-                    {movieDetails.title}
-                </Typography>
-                <Typography className="detailsText" variant="subtitle1" color="text.secondary" component="div">
-                    {movieDetails.description}
-                </Typography>
-                <Typography className="genres" variant="h6" color="text.secondary" component="div">
-                    Genres:
-                </Typography>
-                </CardContent>
-                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+        <div className="detailCard">
+            <Card  sx={{ display: 'flex'}}>
+                <Box className="poster" sx={{ display: 'flex', flexDirection: 'column'}}>
+                <img src={movieDetails.poster}/>
                 </Box>
-            </Box>
-        
-        </Card>
+                <Box sx={{ display: 'flex', flexDirection: 'column'}}>
+                    <CardContent sx={{ flex: '1 0 auto' }}>
+                    <Typography component="div" variant="h4">
+                        {movieDetails.title}
+                    </Typography>
+                    <Typography className="detailsText" variant="subtitle1" color="text.secondary" component="div">
+                        {movieDetails.description}
+                    </Typography>
+                    <Typography className="genres" variant="h6" color="text.secondary" component="div">
+                        Genres: {}
+                    </Typography>
+                    </CardContent>
+                </Box>
+            </Card>
+        </div>
     )
 }
